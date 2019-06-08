1c3f7fc0e6156fe4c1503c1dc3e9fbf5e62eb582
BAANG <chcelestino@gmail.com>
2019-06-08 01:21:01 -0700

    Link moment.js; Create basic DOM structure

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCP-9Xzy5U9DrewOg_I_rF--V0uHf6ocns",
+    authDomain: "train-scheduler-782bf.firebaseapp.com",
+    databaseURL: "https://train-scheduler-782bf.firebaseio.com",
+    projectId: "train-scheduler-782bf",
+    storageBucket: "",
+    messagingSenderId: "820568229937",
+    appId: "1:820568229937:web:3162fa840d6f09ba"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
