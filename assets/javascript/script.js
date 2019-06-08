6998343c9c208e9ada5532477eba8394bdd077b8
BAANG <chcelestino@gmail.com>
2019-06-08 14:37:10 -0700

    Create functioning firebase

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,7 @@ var firebaseConfig = {
     authDomain: "train-scheduler-782bf.firebaseapp.com",
     databaseURL: "https://train-scheduler-782bf.firebaseio.com",
     projectId: "train-scheduler-782bf",
-    storageBucket: "",
+    storageBucket: "train-scheduler-782bf.appspot.com",
     messagingSenderId: "820568229937",
     appId: "1:820568229937:web:3162fa840d6f09ba"
   };
@@ -11,3 +11,51 @@ var firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
 
   var database = firebase.database();
+
+  //Initial Values
+  var trainName = "";
+  var destination = "";
+  var firstTrain;
+  var frequency = 0;
+
+
+  //Click event listener to capture user input
+  $("#submitTrain").on("click", function(event) {
+    event.preventDefault();
+
+  trainName = $("#train-name").val().trim();
+  destination = $("#destination").val().trim();
+  firstTrain = $("#first-train").val().trim();
+  frequency = $("#frequency").val().trim();
+
+  console.log(trainName)
+  console.log(destination)
+  console.log(firstTrain)
+  console.log(frequency)
+
+    //Push values to database
+    database.ref().push({
+      trainName: trainName,
+      destination: destination,
+      firstTrain: firstTrain,
+      frequency: frequency
+    })
+
+  $("#add-train-form").each(function (){ //form reset on submit
+    this.reset();
+  })
+
+  });
+
+
+
+
+  var tr = $("<tr>")
+
+
+  tr.append("<td>" + trainName + "</td>")
+  tr.append("#destination")
+  tr.append("#firstTrain")
+  tr.append("#frequency")
+
+  $("#tableBody").append(tr)
